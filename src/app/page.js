81e3494ba5e91f787afe32d85008b6f7cefa10c1
esharper87 <Elginsharper87@gmail.com>
2024-06-20 21:12:05 -0500

    finish

---
+++ src/app/page.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+const Home = () => (
+
+ <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+
+ <h1 className="mb-6 text-4xl font-bold text-blue-600">
+
+ Welcome to Recipe Page
+
+ </h1>
+
+ <p className="mb-6 text-lg text-gray-700">
+
+ This is the home page of The Recipe's from Elgin Sharper.
+
+ </p>
+
+ </div>
+
+);
+
+
+
+export default Home;
